099d88161825e52a2c27019ca7deb9a840568016
ayaankhan98 <ayaankhan98@gmail.com>
2019-12-24 13:07:21 +0530

    updated

--- create.py
+++ create.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:3gk9yy=l@localhost:5432/users"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://qrpuxrpigcvxar:26bde601461b37b92590a274ba54a0b733036b964e43ec8a3e4698e3c7df8fd3@ec2-174-129-254-218.compute-1.amazonaws.com:5432/dadud12e7137fa"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
